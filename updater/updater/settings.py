2a74737ecaeafa9a62ef090b2e0f2f867f3e0c43
baljka baljka <baljinnyambaljka5@gmail.com>
2025-08-12 11:03:27 +0800

    change: settings.py base dir

--- updater/updater/settings.py
+++ updater/updater/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -82,6 +82,10 @@ DATABASES = {
         'PASSWORD': 'Change_123',
         'HOST': '10.10.90.34',
         'PORT': 3306,
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            # 'use_unicode': False,
+        },
     }
 }
 
@@ -122,8 +126,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
